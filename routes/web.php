3e9ca55fdf8e6d9bc30a61835d55e4d4a2e255d2
3B232073 <3b232073@gm.student.ncut.edu.tw>
2025-12-24 10:29:19 +0800

    設置 products 產生7個路由路由並新增註解

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,23 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-
+Route::resource('project',ProductController::class);
+/*
+  products.index    GET                  /                       ProductController@index
+                    列出所有產品
+  products.store    POST                products                 ProductController@store
+                    儲存新增的產品
+  products.create   GET                 products                 ProductController@create
+                    產生產品新增的表單
+  products.show     GET|HEAD            products/{product}       ProductController@show
+                    檢視某一產品
+  products.update   PUT|PATCH           products/{product}       ProductController@update
+                    更新某一產品
+  products.destroy  DELETE              products/{product}       ProductController@destroy
+                    刪除某一產品
+  products.edit     GET|HEAD            products/{product}/edit  ProductController@edit
+                    產生某一品牌修改的表單
+*/
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
